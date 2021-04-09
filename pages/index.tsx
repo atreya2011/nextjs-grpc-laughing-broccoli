4cd2cea6e331d39a9f5a4264fc1a66a63b4fce4e
Atreya <44151328+atreya2011@users.noreply.github.com>
2021-04-09 13:07:05 +0900

    feat: use protobuf-ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
-import { UserServiceClient } from "@atreya2011/grpc-client-laughing-brocolli/example_grpc_pb";
-import { AddUserRequest, AddUserResponse } from "@atreya2011/grpc-client-laughing-brocolli/example_pb";
-import { credentials, Metadata } from "@grpc/grpc-js";
-import { GetServerSideProps } from "next";
+import { AddUserRequest, UserServiceClient } from "@atreya2011/grpc-protobuf-ts-laughing-brocolli/example";
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+import { RpcOptions, UnaryCall } from "@protobuf-ts/runtime-rpc";
 import Head from "next/head";
+import { useState } from "react";
 
-interface HomeProps {
-  userId: string;
-}
+export default function Home() {
+  const [userId, setUserId] = useState("");
 
-export default function Home({ userId }: HomeProps) {
+  const genUserId = async () => {
+    const options: RpcOptions = {
+      interceptors: [
+        {
+          // adds auth header to unary requests
+          interceptUnary(next, method, input, options: RpcOptions): UnaryCall {
+            if (!options.meta) {
+              options.meta = {};
+            }
+            options.meta["Authorization"] =
+              "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSiBTbWl0aCIsImVtYWlsIjoianNtaXRoQGV4YW1wbGUuY29tIiwiaWF0IjoxNjEzMzUwNTAxfQ.g_Ad8T49-PeF0PGWGcYSjlG0Ib3jBHF52bqoMaEXj6td7SlCAJBwtH8UdsFVAX6xT44DQvydOidLI-vgKP9E_Q";
+            return next(method, input, options);
+          },
+        },
+      ],
+    };
+
+    const transport = new GrpcWebFetchTransport({
+      ...options,
+      baseUrl: "http://localhost:10001",
+    });
+
+    const client = new UserServiceClient(transport);
+    const addUserRequest = {} as AddUserRequest;
+    const { response } = await client.addUser(addUserRequest);
+    setUserId(response.user?.id as string);
+  };
+
   return (
     <div>
       <Head>
@@ -23,31 +49,10 @@ export default function Home({ userId }: HomeProps) {
         <p className="p-3 text-xl">
           Generated User ID is: <strong>{userId}</strong>
         </p>
+        <button className="flex ml-3 bg-indigo-600 text-white rounded-md p-2 hover:bg-indigo-900" onClick={genUserId}>
+          Generate User ID
+        </button>
       </main>
     </div>
-  );
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await AddUser();
-  return {
-    props: {
-      userId: res.user?.id,
-    },
-  };
-};
-
-function AddUser() {
-  const request = new AddUserRequest();
-  const client: UserServiceClient = new UserServiceClient("localhost:10000", credentials.createInsecure());
-
-  const metadata: Metadata = new Metadata();
-  metadata.add(
-    "Authorization",
-    "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSiBTbWl0aCIsImVtYWlsIjoianNtaXRoQGV4YW1wbGUuY29tIiwiaWF0IjoxNjEzMzUwNTAxfQ.g_Ad8T49-PeF0PGWGcYSjlG0Ib3jBHF52bqoMaEXj6td7SlCAJBwtH8UdsFVAX6xT44DQvydOidLI-vgKP9E_Q",
-  );
-
-  return new Promise<AddUserResponse.AsObject>((resolve, reject) =>
-    client.addUser(request, metadata, (err, user) => (err ? reject(err) : resolve(user.toObject()))),
   );
 }
